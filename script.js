f97a47a592a898efd213259da12ffbfb450b0b7b
CrackTaube <50035000+CrackTaube@users.noreply.github.com>
2021-11-18 20:20:16 +0100

    New

--- script.js
+++ script.js
@@ -24,7 +24,32 @@ function searchFromLandingPage() {
 
 }
 
+function searchFromListingPage() {
+  /*location.href="./finder.html"*/
+  let used_form = document.getElementById("search_listing");
+  searched_currency = used_form.elements[0].value;
+  
+  /*fiat_currency = document.querySelector("input[name='fiat_currency']:checked").value;*/
 
+  alert("Getting " + searched_currency ) /* + "/" + fiat_currency)*/
+
+}
+
+alert("Sup! Boi")
+
+fetch("https://coingecko.p.rapidapi.com/simple/price?ids=BTC&vs_currencies=EUR", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "coingecko.p.rapidapi.com",
+		"x-rapidapi-key": "fdc94e748amsh81a55b72b4da354p16f16bjsnf20364f17e8e"
+	}
+})
+.then(response => {
+	console.log(response);
+})
+.catch(err => {
+	console.error(err);
+});
 
 
 
